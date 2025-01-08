b7564e7cdeb52ca6a4a06d61d393c544b9b1b887
geffrys <geffry.ospina@gmail.com>
2025-01-08 00:37:19 -0500

    Project structure finished.

---
+++ GestionTiendaVirtual/tienda-virtual/src/test/java/com/geffry/tienda_virtual/TiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geffry.tienda_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
